8151e84430a699b84eb3fbfac729c8c9cc01475c
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-01 05:39:35 -0400

    Se agregaron validaciones para comprobar que el nuevo registro de usuario no sea un nombre repetido, se implementó el uso de like y los %% que se deben colocar en los parámetros y no en la consulta preparada, se creó la validación para agregar usuarios, que impide el acceso a no ser que se haga una petición POST

--- database/usuarios.php
+++ database/usuarios.php
@@ -1,5 +1,5 @@
 <?php
-// ---------------- Crea un nuevo registro en la tabla colores - CREATE
+// ---------------- Crea un nuevo registro en la tabla usuarios - CREATE
 $crearUsuario = function($request) use ($link, $user, $password, $cerrarConexiones) {
     try {
         $pdo = new PDO($link, $user, $password);
@@ -11,6 +11,30 @@ $crearUsuario = function($request) use ($link, $user, $password, $cerrarConexion
         $stm->execute($request);
 
         $result = "200";
+    } 
+    catch (PDOException $e) {
+        $result = $e->getMessage();
+    }
+    finally {
+        $cerrarConexiones($pdo, $stm);
+    }
+
+    return $result;
+};
+
+// ---------------- Comprueba si el nombre de usuario existe en la BBDD - READ
+$validarNombreUsuario = function($nombre) use ($link, $user, $password, $cerrarConexiones) {
+    try {
+        $pdo = new PDO($link, $user, $password);
+
+        $sql = "SELECT * FROM usuarios WHERE nombre LIKE ?";
+
+        $stm = $pdo->prepare($sql);
+
+        // ---------------- Forma de agregar LIKE a una consulta preparada
+        $stm->execute(["%$nombre%"]);
+
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
     } 
     catch (PDOException $e) {
         $result = $e->getMessage();
--- form-sesion/agregar_usuario.php
+++ form-sesion/agregar_usuario.php
@@ -3,26 +3,34 @@ require_once "../config/variables-conexion.php";
 require_once "../database/globals.php";
 require_once "../database/usuarios.php";
 
-// --------- Crear un array para la consulta preparada y encriptar la clave del usuario
-$request = [
-    "usuario"     => $_POST["nombre_usuario"],
-    "contrasena"  => password_hash($_POST["contrasena"], PASSWORD_BCRYPT) 
-];
+if($_POST) {
+    // --------- Crear un array para la consulta preparada y encriptar la clave del usuario
+    $request = [
+        "usuario"     => $_POST["nombre_usuario"],
+        "contrasena"  => password_hash($_POST["contrasena"], PASSWORD_BCRYPT) 
+    ];
 
-// --------- Almacenar la clave de confirmación para comprobar que coinciden
-$contrasena_verificacion = $_POST["contrasena2"];
+    // --------- Almacenar la clave de confirmación para comprobar que coinciden
+    $contrasena_verificacion = $_POST["contrasena2"];
 
-// --------- La etiqueta <pre></pre> formatea el dontenido de su interior
-echo "<pre>";
-var_dump($request);
-var_dump($contrasena_verificacion);
-echo "</pre>";
+    $result = $validarNombreUsuario($request["usuario"]);
 
-// --------- Comprobar que las contraseñas coinciden
-if(password_verify($contrasena_verificacion, $request["contrasena"])) {
-    $crearUsuario($request);
-    header('Location: ../index.php');
+    if(!$result) {
+        // --------- Comprobar que las contraseñas coinciden
+        if(password_verify($contrasena_verificacion, $request["contrasena"])) {
+            $crearUsuario($request);
+            header('Location: ../index.php');
+        }
+        else {
+            header('Location: registro.php');
+        }
+    }
+    else {
+        echo "<h1>Este usuario ya existe</h1>";
+        die();
+    }
 }
 else {
-    header('Location: registro.php');
+    echo "<h1>No autorizado</h1>";
+    die();
 }
