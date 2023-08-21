f7a5b966d33943d6d7177eb05387ad4e9d756c3e
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-21 05:13:25 -0400

    Renombrada la BBDD

--- config/variables-conexion.php
+++ config/variables-conexion.php
@@ -1,11 +1,11 @@
 <?php
 // ---------------- Variables propias de PDO
 $driver   = "mysql";
-$link     = "mysql:host=localhost;dbname=cursos_php_colores_crud";
+$link     = "mysql:host=localhost;dbname=cursos_php_mh_app_colores";
 
 // ---------------- Variables compartidas por PDO y MySQL
 $host     = "localhost";
-$db       = "cursos_php_colores_crud";
+$db       = "cursos_php_mh_app_colores";
 $user     = "root";
 $password = "";
 
